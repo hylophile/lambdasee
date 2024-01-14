4092d2e34c6570bb72a127acbd2be36b031f372d
hylo <hylo@posteo.de>
2024-01-14 14:15:13 +0100

    feat: add lambdaC syntax

--- app/Main.hs
+++ app/Main.hs
@@ -5,48 +5,53 @@ import Text.ParserCombinators.Parsec
 
 main :: IO ()
 main = do
-    putStrLn $ readExpr "(\\a.(y u))"
-    putStrLn $ readExpr "(λα.(y u))"
-    putStrLn $ readExpr "(λα . (y u))"
+    putStrLn $ readExpr "(\\a:B.(y u))"
+    putStrLn $ readExpr "(λx:α.(y u))"
+    putStrLn $ readExpr "(λy : α . (y u))"
     putStrLn $ readExpr "(y u)"
     putStrLn $ readExpr "ident"
-
+    putStrLn $ readExpr "(λα : ∗ . (Πx:α.bottom))A" -- TODO not exhaustive ???
+    putStrLn $ readExpr "((λα : ∗ . (Πx:α.bottom)) A)"
 {- $>
 main
 <$
 -}
 
--- newtype AIdentifier = String
+-- newtype AVariable = String
 -- data LambdaExpr where
---     Abstraction :: Identifier -> LambdaExpr -> LambdaExpr
---     Identifier :: String -> LambdaExpr
+--     LambdaAbstraction :: Variable -> LambdaExpr -> LambdaExpr
+--     Variable :: String -> LambdaExpr
 --     Application :: LambdaExpr -> LambdaExpr -> LambdaExpr
 data LambdaExpr
-    = Identifier String
-    | Abstraction String LambdaExpr
+    = Variable String
+    | Box
+    | Star
     | Application LambdaExpr LambdaExpr
+    | LambdaAbstraction LambdaExpr (Maybe LambdaExpr) LambdaExpr
+    | PiAbstraction LambdaExpr (Maybe LambdaExpr) LambdaExpr
     deriving (Show)
 
--- parseAbstraction :: Parser LambdaExpr
--- parseAbstraction =
-
-parseIdentifier :: Parser LambdaExpr
-parseIdentifier = do
+parseVariable :: Parser LambdaExpr
+parseVariable = do
     x <- many1 letter
-    return $ Identifier x
+    return $ Variable x
 
-parseAbstraction :: Parser LambdaExpr
-parseAbstraction = do
-    _ <- char '('
-    _ <- char '\\' <|> char 'λ'
-    x <- many1 letter
-    _ <- spaces
-    _ <- char '.'
-    _ <- spaces
-    body <- parseExpr
-    _ <- char ')'
-    return $ Abstraction x body
+parseBox :: Parser LambdaExpr
+parseBox = do
+    _ <-
+        char '!' -- Firefox & Chrome
+            <|> char '□'
+            <|> char '#'
+    return Box
 
+parseStar :: Parser LambdaExpr
+parseStar = do
+    _ <- char '∗' <|> char '*'
+    return Star
+
+-- ∅(∗ : !
+-- E = V | ! | ∗ |(EE)|(λV : E . E)|(ΠV : E . E) .
+-- {(∗, ∗), (!, ∗)},
 parseApplication :: Parser LambdaExpr
 parseApplication = do
     _ <- char '('
@@ -56,10 +61,55 @@ parseApplication = do
     _ <- char ')'
     return $ Application a b
 
+parseLambdaAbstraction :: Parser LambdaExpr
+parseLambdaAbstraction = do
+    _ <- char '('
+    _ <- char '\\' <|> char 'λ'
+    x <- parseExpr -- TODO only Variable
+    -- TODO type optional?
+    _ <- spaces
+    _ <- char ':'
+    _ <- spaces
+    xType <- parseExpr
+    -- type end
+    _ <- spaces
+    _ <- char '.'
+    _ <- spaces
+    body <- parseExpr
+    _ <- char ')'
+    return $ LambdaAbstraction x (Just xType) body
+
+-- TODO arrow notation: A → B for Πx : A . B,  in case x \not\in FV (B)
+parsePiAbstraction :: Parser LambdaExpr
+parsePiAbstraction = do
+    _ <- char '('
+    _ <- char 'Π' <|> char '/'
+    x <- parseExpr -- TODO only Variable
+    -- TODO type optional?
+    _ <- spaces
+    _ <- char ':'
+    _ <- spaces
+    xType <- parseExpr
+    -- type end
+    _ <- spaces
+    _ <- char '.'
+    _ <- spaces
+    body <- parseExpr
+    _ <- char ')'
+    return $ PiAbstraction x (Just xType) body
+
 parseExpr :: Parser LambdaExpr
-parseExpr = parseIdentifier <|> try parseAbstraction <|> parseApplication
+parseExpr =
+    parseVariable
+        <|> parseBox
+        <|> parseStar
+        <|> try parseApplication
+        <|> try parseLambdaAbstraction
+        <|> parsePiAbstraction
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right v -> show v
+
+-- TODO parseJudgement
