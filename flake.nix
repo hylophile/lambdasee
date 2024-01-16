2de3257f3fa4e89863c39739d969df16ba561bd8
hylo <hylo@posteo.de>
2024-01-16 13:27:26 +0100

    feat: implement most parsing with pest

--- flake.nix
+++ flake.nix
@@ -1,24 +1,23 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
-  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.parts.url = "github:hercules-ci/flake-parts";
-  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-
-  outputs = inputs@{ parts, nci, ... }:
-    parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      imports = [ nci.flakeModule ./crates.nix ];
-      perSystem = { pkgs, config, ... }:
-        let
-          # shorthand for accessing this crate's outputs
-          # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
-          crateOutputs = config.nci.outputs."lambdasee";
-        in {
-          # export the crate devshell as the default devshell
-          devShells.default = crateOutputs.devShell;
-          # export the release package of the crate as default package
-          packages.default = crateOutputs.packages.release;
-        };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
+          ./rust-toolchain.toml;
+      in with pkgs; {
+        devShells.default = mkShell { buildInputs = [ rustToolchain ]; };
+      });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+extern crate pest;
+
 use pest::iterators::Pairs;
 use pest::pratt_parser::PrattParser;
 use pest::Parser;
-use std::io::{self, BufRead};
+use std::io::{self};
 
 #[derive(pest_derive::Parser)]
-#[grammar = "calculator.pest"]
+#[grammar = "grammar.pest"]
 pub struct CalculatorParser;
 
 lazy_static::lazy_static! {
@@ -15,37 +17,72 @@ lazy_static::lazy_static! {
         // Precedence is defined lowest to highest
         PrattParser::new()
             // Addition and subtract have equal precedence
-            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
-            .op(Op::infix(multiply, Left) | Op::infix(divide, Left) | Op::infix(modulo, Left))
-            .op(Op::prefix(unary_minus))
+            .op(Op::infix(appl, Left))
+            .op(Op::infix(arrow, Right))
     };
 }
 
 #[derive(Debug)]
 pub enum Expr {
-    Integer(i32),
-    UnaryMinus(Box<Expr>),
+    Ident(String),
+    Star,
+    Box,
     BinOp {
         lhs: Box<Expr>,
         op: Op,
         rhs: Box<Expr>,
     },
+    LambdaAbstraction {
+        ident: Box<Expr>,
+        etype: Box<Expr>,
+        body: Box<Expr>,
+    },
+    PiAbstraction {
+        ident: Box<Expr>,
+        etype: Box<Expr>,
+        body: Box<Expr>,
+    },
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Application,
+    Arrow,
 }
 
 pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
+    // println!("{}", pairs);
     PRATT_PARSER
         .map_primary(|primary| match primary.as_rule() {
-            Rule::integer => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
+            // Rule::integer => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
+            Rule::star => Expr::Star,
+            Rule::lambda => {
+                let mut inner = primary.into_inner();
+                let ident = Box::new(parse_expr(pest::iterators::Pairs::single(
+                    inner.next().unwrap(),
+                )));
+                let etype = Box::new(parse_expr(inner.next().unwrap().into_inner()));
+                let body = Box::new(parse_expr(inner.next().unwrap().into_inner()));
+                Expr::LambdaAbstraction { ident, etype, body }
+            }
+            Rule::pi => {
+                let mut inner = primary.into_inner();
+                let ident = Box::new(parse_expr(pest::iterators::Pairs::single(
+                    inner.next().unwrap(),
+                )));
+                let etype = Box::new(parse_expr(inner.next().unwrap().into_inner()));
+                let body = Box::new(parse_expr(inner.next().unwrap().into_inner()));
+                Expr::PiAbstraction { ident, etype, body }
+            }
             Rule::expr => parse_expr(primary.into_inner()),
+            Rule::ident => Expr::Ident(primary.as_str().into()),
             rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
         })
+        // .map_body
         .map_infix(|lhs, op, rhs| {
             let op = match op.as_rule() {
-                Rule::add => Op::Add,
-                Rule::subtract => Op::Subtract,
-                Rule::multiply => Op::Multiply,
-                Rule::divide => Op::Divide,
-                Rule::modulo => Op::Modulo,
+                Rule::arrow => Op::Arrow, // TODO convert to PiAbstraction
+                Rule::appl => Op::Application,
                 rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
             };
             Expr::BinOp {
@@ -54,36 +91,40 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
                 rhs: Box::new(rhs),
             }
         })
-        .map_prefix(|op, rhs| match op.as_rule() {
-            Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
-            _ => unreachable!(),
-        })
+        // .map_prefix(|op, rhs| match op.as_rule() {
+        //     Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
+        //     _ => unreachable!(),
+        // })
         .parse(pairs)
 }
 
-#[derive(Debug)]
-pub enum Op {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Modulo,
-}
-
 fn main() -> io::Result<()> {
-    for line in io::stdin().lock().lines() {
-        match CalculatorParser::parse(Rule::equation, &line?) {
-            Ok(mut pairs) => {
-                println!(
-                    "Parsed: {:#?}",
-                    // inner of expr
-                    parse_expr(pairs.next().unwrap().into_inner())
-                );
-            }
-            Err(e) => {
-                eprintln!("Parse failed: {:?}", e);
-            }
+    let line = "(Πx : S . (A → P x)) → A → Πy : S . P y";
+    // let line = "∅ ⊢ λα : ∗ . λβ : (∗ → ∗) . β(β α) : ∗ → (∗ → ∗) → ∗";
+    // let line = "α : ∗ . λβ : ∗ . α → β : e";
+    // let line = "(λα : ∗ . α → α) (γ → β)";
+    // let line = "(λα : ∗ . α → α) γ";
+    // let line = "(Πy : S . P y)";
+    // let line = "a -> b";
+    // let line = "a";
+    // let line = "a b";
+    // let line = "a b c";
+
+    match CalculatorParser::parse(Rule::program, &line) {
+        Ok(mut pairs) => {
+            // let a = pairs.next().unwrap();
+            println!("p: {pairs}");
+            let a = pairs.next().unwrap().into_inner();
+            println!("a: {a}");
+            println!(
+                "Parsed: \n\n{:#?}",
+                // inner of expr
+                parse_expr(a)
+            );
         }
+        Err(e) => {
+            eprintln!("Parse failed: {:?}", e);
+        } // }
     }
     Ok(())
 }
